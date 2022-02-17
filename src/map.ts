268ba09e391fa9bab63b0f26d60984ee32d20e95
Anton Khorev <tony29@yandex.ru>
2022-02-17 19:25:38 +0300

    encode note state changes in markers

--- src/map.ts
+++ src/map.ts
@@ -1,21 +1,34 @@
-import type {Note} from './data'
+import type {Note, NoteComment} from './data'
 
 export class NoteMarker extends L.Marker {
 	noteId: number
 	constructor(note: Note) {
 		const width=25
 		const height=40
+		const nInnerCircles=4
 		const r=width/2
 		const rp=height-r
 		const y=r**2/rp
 		const x=Math.sqrt(r**2-y**2)
 		const xf=x.toFixed(2)
 		const yf=y.toFixed(2)
-		const html=
-			`<svg xmlns="http://www.w3.org/2000/svg" viewBox="${-r} ${-r} ${width} ${height}">`+
-			//`<path d="M0,${rp} L${-r},0 L0,${-r} L${r},0 Z" fill="${note.status=='open'?'red':'green'}" />`+
-			`<path d="M0,${rp} L-${xf},${yf} A${r},${r} 0 1 1 ${xf},${yf} Z" fill="${note.status=='open'?'red':'green'}" />`+
-			`</svg>`
+		const dcr=(r-.5)/nInnerCircles
+		let html=``
+		html+=`<svg xmlns="http://www.w3.org/2000/svg" viewBox="${-r} ${-r} ${width} ${height}">`
+		html+=`<path d="M0,${rp} L-${xf},${yf} A${r},${r} 0 1 1 ${xf},${yf} Z" fill="${note.status=='open'?'red':'green'}" />`
+		const states=[...noteCommentsToStates(note.comments)]
+		const statesToDraw=states.slice(-nInnerCircles,-1)
+		for (let i=2;i>=0;i--) {
+			if (i>=statesToDraw.length) continue
+			const cr=dcr*(i+1)
+			html+=`<circle r="${cr}" fill="${color()}" stroke="white" />`
+			function color(): string {
+				if (i==0 && states.length<=nInnerCircles) return 'white'
+				if (statesToDraw[i]) return 'red'
+				return 'green'
+			}
+		}
+		html+=`</svg>`
 		const icon=L.divIcon({
 			html,
 			className: '',
@@ -81,5 +94,17 @@ export class NoteMap extends L.Map {
 	}
 	fitNoteTrack(): void {
 		this.fitBounds(this.trackLayer.getBounds())
+	}
+}
+
+function *noteCommentsToStates(comments: NoteComment[]): Iterable<boolean> {
+	let currentState=true
+	for (const comment of comments) {
+		if (comment.action=='opened' || comment.action=='reopened') {
+			currentState=true
+		} else if (comment.action=='closed' || comment.action=='hidden') {
+			currentState=false
+		}
+		yield currentState
 	}
 }
