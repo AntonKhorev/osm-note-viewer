11b89810716d3416fcc7ba8673eff07f7e59ee58
Anton Khorev <tony29@yandex.ru>
2022-02-18 19:47:37 +0300

    date ranges ignoring created/updated

--- src/query.ts
+++ src/query.ts
@@ -35,11 +35,27 @@ export interface NoteFetchDetails {
                             display_name, sort, order - these don't change within a query;
                             closed - this may change between phases;
                             limit - this may change within a phase in rare circumstances;
-                            from, to - this change for pagination purposes, only one of them is present
+                            from, to - this change for pagination purposes, from needs to be present with a dummy date if to is used
  */
 export function getNextFetchDetails(query: NoteQuery, allNotes: Note[], lastBatchNotes: Note[]): NoteFetchDetails {
 	let closed=-1
 	if (query.status=='open') closed=0
+	let lowerDateLimit:string|undefined
+	let upperDateLimit:string|undefined
+	if (allNotes.length>0) {
+		const lastNote=allNotes[allNotes.length-1]
+		if (lastNote.comments.length>0) {
+			const lastComment=lastNote.comments[lastNote.comments.length-1]
+			if (query.order=='oldest') {
+				lowerDateLimit=makeLowerLimit(lastComment.date)
+			} else {
+				upperDateLimit=makeUpperLimit(lastComment.date)
+			}
+		}
+	}
+	if (lowerDateLimit==null && upperDateLimit!=null) {
+		lowerDateLimit='2001-01-01T00:00:00Z'
+	}
 	const parameters:Array<[string,string|number]>=[
 		['display_name',query.user],
 		['sort',query.sort],
@@ -47,8 +63,24 @@ export function getNextFetchDetails(query: NoteQuery, allNotes: Note[], lastBatc
 		['closed',closed],
 		['limit',query.limit]
 	]
+	if (lowerDateLimit!=null) parameters.push(['from',lowerDateLimit])
+	if (upperDateLimit!=null) parameters.push(['to',upperDateLimit])
 	return {
 		parameters: parameters.map(([k,v])=>k+'='+encodeURIComponent(v)).join('&'),
 		autorun: true
 	}
+}
+
+function makeLowerLimit(dateInSeconds: number): string {
+	return makeISODateString(dateInSeconds)
+}
+
+function makeUpperLimit(dateInSeconds: number): string {
+	return makeISODateString(dateInSeconds+1)
+}
+
+function makeISODateString(dateInSeconds: number): string {
+	const dateObject=new Date(dateInSeconds*1000)
+	const dateString=dateObject.toISOString()
+	return dateString.replace(/.\d\d\dZ$/,'Z')
 }
--- test/query.js
+++ test/query.js
@@ -24,4 +24,60 @@ describe("query module",()=>{
 		assert(fd.autorun)
 		assert.equal(fd.parameters,`display_name=SomeOne&sort=created_at&order=newest&closed=0&limit=23`)
 	})
+	it("provides subsequent fetch for newest-first order",()=>{
+		const makeNote=(id,date)=>({
+			id,
+			lat: 60,
+			lon: 30,
+			status: 'open',
+			comments: [{
+				date,
+				action: 'opened',
+				text: 'Hello!'
+			}]
+		})
+		const notes=[
+			makeNote(1,1645398621),
+			makeNote(2,1645298621),
+			makeNote(3,1645198621) // 2022-02-18T15:37:01Z
+		]
+		const fd=getNextFetchDetails({
+			user: 'Dude',
+			status: 'mixed',
+			sort: 'created_at',
+			order: 'newest',
+			limit: 3
+		},notes,notes)
+		assert(fd.autorun)
+		assert.equal(fd.parameters,`display_name=Dude&sort=created_at&order=newest&closed=-1&limit=3&from=2001-01-01T00%3A00%3A00Z&to=2022-02-18T15%3A37%3A02Z`)
+	})
+	it("provides subsequent fetch for newest-first order",()=>{
+		const makeNote=(id,date)=>({
+			id,
+			lat: 60,
+			lon: 30,
+			status: 'open',
+			comments: [{
+				date,
+				action: 'opened',
+				text: 'Hello!'
+			}]
+		})
+		const notes=[
+			makeNote(1,1643198621),
+			makeNote(2,1644198621),
+			makeNote(3,1645198621) // 2022-02-18T15:37:01Z
+		]
+		const fd=getNextFetchDetails({
+			user: 'Dude',
+			status: 'mixed',
+			sort: 'created_at',
+			order: 'oldest',
+			limit: 3
+		},notes,notes)
+		assert(fd.autorun)
+		assert.equal(fd.parameters,`display_name=Dude&sort=created_at&order=oldest&closed=-1&limit=3&from=2022-02-18T15%3A37%3A01Z`)
+	})
+	// TODO test multiple comments created sort
+	// TODO test multiple comments updated sort
 })
