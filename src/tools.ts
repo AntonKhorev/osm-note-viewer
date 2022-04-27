6fafc08555d27848ac55d0b063bbadefc4dd895e
Anton Khorev <tony29@yandex.ru>
2022-04-28 00:39:24 +0300

    tools rewrite: street view tools

--- src/tools.ts
+++ src/tools.ts
@@ -454,50 +454,60 @@ class GpxTool extends Tool {
 	}
 }
 
+abstract class StreetViewTool extends Tool {
+	getTool(callbacks: ToolCallbacks, map: NoteMap): ToolElements {
+		const $viewButton=document.createElement('button')
+		$viewButton.append(`Open `,makeMapIcon('center'))
+		$viewButton.addEventListener('click',()=>{
+			open(this.generateUrl(map),this.id)
+		})
+		return [$viewButton]
+	}
+	protected abstract generateUrl(map: NoteMap): string
+}
+
+class YandexPanoramasTool extends StreetViewTool {
+	constructor() {super(
+		'yandex-panoramas',
+		`Y.Panoramas`,
+		`Yandex.Panoramas (Яндекс.Панорамы)`
+	)}
+	getInfo() {return[p(
+		`Open a map location in `,makeLink(`Yandex.Panoramas`,'https://wiki.openstreetmap.org/wiki/RU:%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F/%D0%AF%D0%BD%D0%B4%D0%B5%D0%BA%D1%81.%D0%9F%D0%B0%D0%BD%D0%BE%D1%80%D0%B0%D0%BC%D1%8B'),` street view. `,
+		`Could be useful to find out if an object mentioned in a note existed at a certain point of time. `,
+		`Yandex.Panoramas have a year selector in the upper right corner. Use it to get a photo made close to the date of interest.`
+	)]}
+	protected generateUrl(map: NoteMap): string {
+		const e=makeEscapeTag(encodeURIComponent)
+		const coords=map.lon+','+map.lat
+		return e`https://yandex.ru/maps/?ll=${coords}&panorama%5Bpoint%5D=${coords}&z=${map.zoom}` // 'll' is required if 'z' argument is present
+	}
+}
+
+class MapillaryTool extends StreetViewTool {
+	constructor() {super(
+		'mapillary',
+		`Mapillary`
+	)}
+	getInfo() {return[p(
+		`Open a map location in `,makeLink(`Mapillary`,'https://wiki.openstreetmap.org/wiki/Mapillary'),`. `,
+		`Not yet fully implemented. The idea is to jump straight to the best available photo, but in order to do that, Mapillary API has to be queried for available photos. That's impossible to do without an API key.`
+	)]}
+	protected generateUrl(map: NoteMap): string {
+		const e=makeEscapeTag(encodeURIComponent)
+		return e`https://www.mapillary.com/app/?lat=${map.lat}&lng=${map.lon}&z=${map.zoom}&focus=photo`
+	}
+}
+
 export const toolMakerSequence: Array<()=>Tool> = [
 	()=>new AutozoomTool,
 	()=>new TimestampTool, ()=>new OverpassTurboTool, ()=>new OverpassDirectTool,
 	()=>new RcTool, ()=>new IdTool,
-	()=>new GpxTool
+	()=>new GpxTool, ()=>new YandexPanoramasTool, ()=>new MapillaryTool
 ]
 
 /*
 	static commandGroups: CommandGroup[] = [[
-		'yandex-panoramas',
-		`Y.Panoramas`,
-		`Yandex.Panoramas (Яндекс.Панорамы)`,
-		(cp,map)=>{
-			const $viewButton=document.createElement('button')
-			$viewButton.append(`Open `,makeMapIcon('center'))
-			$viewButton.addEventListener('click',()=>{
-				const e=makeEscapeTag(encodeURIComponent)
-				const coords=map.lon+','+map.lat
-				const url=e`https://yandex.ru/maps/?ll=${coords}&panorama%5Bpoint%5D=${coords}&z=${map.zoom}` // 'll' is required if 'z' argument is present
-				open(url,'yandex')
-			})
-			return [$viewButton]
-		},()=>[p(
-			`Open a map location in `,makeLink(`Yandex.Panoramas`,'https://wiki.openstreetmap.org/wiki/RU:%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F/%D0%AF%D0%BD%D0%B4%D0%B5%D0%BA%D1%81.%D0%9F%D0%B0%D0%BD%D0%BE%D1%80%D0%B0%D0%BC%D1%8B'),` street view. `,
-			`Could be useful to find out if an object mentioned in a note existed at a certain point of time. `,
-			`Yandex.Panoramas have a year selector in the upper right corner. Use it to get a photo made close to the date of interest.`
-		)]
-	],[
-		'mapillary',
-		`Mapillary`,,
-		(cp,map)=>{
-			const $viewButton=document.createElement('button')
-			$viewButton.append(`Open `,makeMapIcon('center'))
-			$viewButton.addEventListener('click',()=>{
-				const e=makeEscapeTag(encodeURIComponent)
-				const url=e`https://www.mapillary.com/app/?lat=${map.lat}&lng=${map.lon}&z=${map.zoom}&focus=photo`
-				open(url,'mapillary')
-			})
-			return [$viewButton]
-		},()=>[p(
-			`Open a map location in `,makeLink(`Mapillary`,'https://wiki.openstreetmap.org/wiki/Mapillary'),`. `,
-			`Not yet fully implemented. The idea is to jump straight to the best available photo, but in order to do that, Mapillary API has to be queried for available photos. That's impossible to do without an API key.`
-		)]
-	],[
 		'counts',
 		`Note counts`,,
 		(cp,map)=>{
