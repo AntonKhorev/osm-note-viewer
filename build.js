8efb0ddb4f792e8c4db5976fd20dbb305df3adac
Anton Khorev <tony29@yandex.ru>
2022-06-22 14:24:21 +0300

    embed tools svgs

--- build.js
+++ build.js
@@ -2,15 +2,42 @@ import fs from 'fs-extra'
 import { rollup } from 'rollup'
 import typescript from '@rollup/plugin-typescript'
 
-// copy files
+// remove previous build
 await fs.remove('dist')
-await fs.copy('src/index.html','dist/index.html')
-await fs.copy('src/index.css','dist/index.css')
+
+// process svgs
+const embedSvgFiles=['tools-map.svg','tools-notes.svg']
+let embeddedStyles=''
+let embeddedSymbols=''
 for (const svgDirEntry of await fs.readdir('src/svg',{withFileTypes:true})) {
 	if (svgDirEntry.isDirectory()) continue
 	const filename=svgDirEntry.name
-	await fs.copy(`src/svg/${filename}`,`dist/${filename}`)
+	if (embedSvgFiles.includes(filename)) {
+		const [style,symbol]=getEmbeddedSvg(
+			filename.split('.')[0],
+			await fs.readFile(`src/svg/${filename}`,'utf-8')
+		)
+		embeddedStyles+=style
+		embeddedSymbols+=symbol
+	} else {
+		await fs.copy(`src/svg/${filename}`,`dist/${filename}`)
+	}
 }
+
+// build index with embedded svgs
+const embeddedSvgs=
+	`<svg class="symbols">\n`+
+	`<style>\n`+
+	embeddedStyles+
+	`</style>\n`+
+	embeddedSymbols+
+	`</svg>`
+const htmlContents=await fs.readFile('src/index.html','utf-8')
+const patchedHtmlContents=htmlContents.replace(`<!-- embed svgs -->`,embeddedSvgs)
+await fs.writeFile('dist/index.html',patchedHtmlContents)
+
+// copy css
+await fs.copy('src/index.css','dist/index.css')
 
 // compile and bundle scripts
 const bundle=await rollup({
@@ -21,3 +48,25 @@ bundle.write({
 	file: "dist/index.js",
 })
 bundle.close()
+
+function getEmbeddedSvg(id,input) {
+	let style=''
+	let symbol=''
+	for (const line of input.split(/\r?\n/)) {
+		let match
+		if (match=line.match(/^<svg.*(viewBox="[^"]*").*>$/)) {
+			const [,viewBox]=match
+			symbol+=`<symbol id="${id}" ${viewBox}>\n`
+		} else if (match=line.match(/^<\/svg>$/)) {
+			symbol+=`</symbol>\n`
+			break
+		} else if (match=line.match(/^<g class="([^"]*)">$/)) {
+			const [,partClass]=match
+			style+=`#${id} .${partClass} { visibility: var(--${id}-${partClass}-part-visibility,hidden); }\n`
+			symbol+=line+'\n'
+		} else {
+			symbol+=line+'\n'
+		}
+	}
+	return [style,symbol]
+}
