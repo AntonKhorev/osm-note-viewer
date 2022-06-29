6b351a41b08e1e0d9458ddddb5f0413bdefb9111
Anton Khorev <tony29@yandex.ru>
2022-06-29 18:21:27 +0300

    faster test builder

--- build-test.js
+++ build-test.js
@@ -3,18 +3,20 @@ import { rollup } from 'rollup'
 import typescript from '@rollup/plugin-typescript'
 
 // compile and bundle scripts to be tested
+const input=[]
 for (const testDirEntry of await fs.readdir('test',{withFileTypes:true})) {
 	if (testDirEntry.isDirectory()) continue
 	const filename=testDirEntry.name
 	const match=filename.match(/^(.*)\.js$/)
 	if (!match) continue
 	const [,script]=match
-	const bundle=await rollup({
-		input: `src/${script}.ts`,
-		plugins: [typescript()]
-	})
-	bundle.write({
-		file: `test-build/${script}.js`,
-	})
-	bundle.close()
+	input.push(`src/${script}.ts`)
 }
+const bundle=await rollup({
+	input,
+	plugins: [typescript()]
+})
+bundle.write({
+	dir: `test-build`
+})
+bundle.close()
