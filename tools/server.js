7597030a69e45b13fbb1037b76342db0fce7d671
Anton Khorev <tony29@yandex.ru>
2023-01-09 07:28:17 +0300

    fake tiles

--- tools/server.js
+++ tools/server.js
@@ -1,17 +1,25 @@
+import * as fs from 'fs/promises'
 import * as http from 'http'
 
-export default (port=0)=>new Promise((resolve)=>{
+export default async function runServer(port=0) {
+	const tileData=await fs.readFile(new URL('./tile.png',import.meta.url))
 	const server=http.createServer(async(request,response)=>{
 		const [pathname,query]=request.url.split(/\?(.*)/)
 		if (pathname=='/') {
 			response.end(`Welcome to fake osm server!`)
 		} else if (pathname.match(/\.png/)) {
-			response.end(`TODO serve blank image`)
+			response.writeHead(200,{
+				'Content-Type':'image/png',
+				'Cache-Control':'public, max-age=604800, immutable',
+			})
+			response.end(tileData)
 		} else {
 			response.writeHead(404)
 			response.end(`Route not defined`)
 		}
-	}).listen(process.env.PORT||0).on('listening',()=>{
-		resolve(server)
 	})
-})
+	await new Promise((resolve)=>{
+		server.listen(port).on('listening',resolve)
+	})
+	return server
+}
