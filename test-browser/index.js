ff2b45e49ac34583abb8e8c1fff739412e93dffa
Anton Khorev <tony29@yandex.ru>
2023-01-10 14:22:51 +0300

    browser test for getting one note comment

--- test-browser/index.js
+++ test-browser/index.js
@@ -5,11 +5,14 @@ import puppeteer from 'puppeteer'
 import runServer from '../tools/server.js'
 import {buildWithTestServer} from '../tools/build.js'
 
+const visible=false
+
 const downloads=await readJson('downloads.json')
 const dstDir='test-build/dist'
 const browserUrl=`${url.pathToFileURL(`${dstDir}/index.html`)}`
 
-describe("browser tests",()=>{
+describe("browser tests",function(){
+	if (visible) this.timeout(0)
 	let server
 	before(async function(){
 		this.timeout(0)
@@ -23,13 +26,22 @@ describe("browser tests",()=>{
 		server.clearNotes()
 	})
 	it("runs basic query",async()=>{
-		// TODO setup server data
-		const browser=await puppeteer.launch()
+		server.setNotes([{
+			"text": "the-only-note-comment"
+		}])
+		const browser=await puppeteer.launch(visible?{
+			headless: false,
+			slowMo: 300
+		}:{})
 		const page=await browser.newPage()
+		const waitForFetchButton=()=>page.waitForXPath(`//button[not(@disabled) and contains(.,"Fetch notes")]`)
+		const hasText=async(text)=>(await page.$x(`//*[contains(text(),"${text}")]`)).length
 		await page.goto(browserUrl)
-		// TODO check that no results are displayed
-		// TODO click fetch button
-		// TODO check results
+		const button=await waitForFetchButton()
+		assert(!await hasText("the-only-note-comment"))
+		await button.click()
+		await page.waitForSelector(`.notes tbody`)
+		assert(await hasText("the-only-note-comment"))
 		browser.close()
 	})
 })
