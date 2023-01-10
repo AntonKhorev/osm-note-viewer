22c08c6e433a2546b9b1477a9eb127855105edba
Anton Khorev <tony29@yandex.ru>
2023-01-10 07:32:06 +0300

    download css images; specify download files directly to avoid unexpected filenames

--- temp-build.js
+++ temp-build.js
@@ -7,7 +7,13 @@ const server=await runServer()
 const serverHost=`127.0.0.1:${server.address().port}`
 const serverUrl=`http://${serverHost}/`
 const dstDir='test-build/dist'
-await build('src',dstDir,'cache',[{
+await build('src',dstDir,'cache',[
+	[`https://unpkg.com/leaflet@1.7.1/dist/leaflet.js`,`leaflet.js`,`sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==`],
+	[`https://unpkg.com/leaflet@1.7.1/dist/leaflet.css`,`leaflet.css`,`sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==`],
+	[`https://unpkg.com/leaflet@1.7.1/dist/images/layers.png`,`images/layers.png`],
+	[`https://unpkg.com/leaflet@1.7.1/dist/images/layers-2x.png`,`images/layers-2x.png`],
+	[`https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png`,`images/marker-icon.png`],
+],[{
 	web: serverUrl,
 	tiles: `${serverUrl}{z}/{x}/{y}.png`,
 	note: `Test server bundled on ${new Date().toISOString()}`
--- tools/build.js
+++ tools/build.js
@@ -1,16 +1,20 @@
 import * as fs from 'fs/promises'
+import * as path from 'path'
 import * as https from 'https'
 import { createHash } from 'crypto'
 import { rollup } from 'rollup'
 import typescript from '@rollup/plugin-typescript'
 import virtual from '@rollup/plugin-virtual'
 
-export default async function build(srcDir,dstDir,cacheDir,serverListConfig) {
+export default async function build(srcDir,dstDir,cacheDir,downloads,serverListConfig) {
 	await cleanupDirectory(dstDir)
 	if (cacheDir) {
 		await fs.mkdir(cacheDir,{recursive:true})
 	}
-	await buildHtml(srcDir,dstDir,cacheDir)
+	if (downloads) {
+		await downloadFiles(dstDir,cacheDir,downloads)
+	}
+	await buildHtml(srcDir,dstDir,downloads)
 	await buildCss(srcDir,dstDir)
 	await buildJs(srcDir,dstDir,serverListConfig)
 }
@@ -36,6 +40,16 @@ export async function buildTest(srcDir,testDir,dstDir) {
 	bundle.close()
 }
 
+async function downloadFiles(dstDir,cacheDir,downloads) {
+	for (const [url,filename,integrity] of downloads) {
+		const dstFilename=`${dstDir}/${filename}`
+		const cacheFilename=`${cacheDir}/${filename}`
+		await downloadFileToCache(url,cacheFilename,integrity)
+		await fs.mkdir(path.dirname(dstFilename),{recursive:true})
+		await fs.copyFile(cacheFilename,dstFilename)
+	}
+}
+
 async function cleanupDirectory(dir) {
 	await fs.mkdir(dir,{recursive:true})
 	for (const filename of await fs.readdir('dist')) {
@@ -43,7 +57,7 @@ async function cleanupDirectory(dir) {
 	}
 }
 
-async function buildHtml(srcDir,dstDir,cacheDir) {
+async function buildHtml(srcDir,dstDir,downloads) {
 	const [embeddedStyles,embeddedSymbols]=await getAllEmbeddedSvgs(srcDir)
 	const embeddedSvgs=
 		`<svg class="symbols">\n`+
@@ -55,8 +69,8 @@ async function buildHtml(srcDir,dstDir,cacheDir) {
 	const favicon=await fs.readFile(`${srcDir}/svg/favicon.svg`,'utf-8')
 	const encodedFavicon=Buffer.from(favicon).toString('base64')
 	let htmlContents=await fs.readFile(`${srcDir}/index.html`,'utf-8')
-	if (cacheDir) {
-		htmlContents=await downloadCdnFiles(dstDir,cacheDir,htmlContents)
+	if (downloads) {
+		htmlContents=await replaceHtmlLinksToDownloads(downloads,htmlContents)
 	}
 	htmlContents=htmlContents
 		.replace(`<body>`,`<body data-build="${new Date().toISOString()}">`)
@@ -138,60 +152,13 @@ function getEmbeddedSvg(id,input) {
 	return [style,symbol]
 }
 
-async function downloadCdnFiles(dstDir,cacheDir,htmlContents) {
-	const regexp=new RegExp(`<(link|script)([^>]*)>`,'g')
-	const downloads=new Map()
-	htmlContents.replace(regexp,(match,tag,attributesString)=>{
+async function replaceHtmlLinksToDownloads(downloads,htmlContents) {
+	const downloadFilenames=new Map(downloads)
+	const htmlTagRegExp=new RegExp(`<(link|script)([^>]*)>`,'g')
+	return htmlContents.replace(htmlTagRegExp,(match,tag,attributesString)=>{
 		const urlAttribute=tag=='link'?'href':'src'
 		const url=readHtmlAttribute(attributesString,urlAttribute)
-		if (url==null) return match
-		const filename=getFilenameFromUrl(url)
-		if (filename==null) return match
-		const integrity=readHtmlAttribute(attributesString,'integrity')
-		downloads.set(url,[filename,integrity])
-		return match
-	})
-	const completeDownloads=new Map()
-	for (const [url,[filename,integrity]] of downloads) {
-		let hash,digest
-		if (integrity) {
-			let algorithm
-			[algorithm,digest]=integrity.split('-')
-			hash=createHash(algorithm)
-		}
-		const dstFilename=`${dstDir}/${filename}`
-		const cacheFilename=`${cacheDir}/${filename}`
-		try {
-			await fs.access(cacheFilename)
-		} catch {
-			process.stdout.write(`downloading ${url} `)
-			const buffer=await new Promise((resolve,reject)=>{
-				https.get(url,response=>{
-					if (response.statusCode!==200) return reject(`download of ${filename} failed with status code ${response.statusCode}`)
-					const chunks=[]
-					response.on('data',chunk=>{
-						process.stdout.write(`.`)
-						if (hash) hash.update(chunk)
-						chunks.push(chunk)
-					})
-					response.on('error',err=>reject(`download of ${filename} failed while streaming with error ${err}`))
-					response.on('end',()=>{
-						if (hash && hash.digest('base64')!=digest) reject(`download of ${filename} failed integrity check`)
-						resolve(Buffer.concat(chunks))
-					})
-				})
-			})
-			process.stdout.write(`done\n`)
-			await fs.writeFile(cacheFilename,buffer)
-		}
-		await fs.copyFile(cacheFilename,dstFilename)
-		completeDownloads.set(url,filename)
-	}
-	return htmlContents.replace(regexp,(match,tag,attributesString)=>{
-		const urlAttribute=tag=='link'?'href':'src'
-		const url=readHtmlAttribute(attributesString,urlAttribute)
-		if (url==null) return match
-		const filename=completeDownloads.get(url)
+		const filename=downloadFilenames.get(url)
 		if (filename==null) return match
 		const updatedAttributesString=replaceHtmlAttribute(
 			['integrity','crossorigin'].reduce(removeHtmlAttribute,attributesString),
@@ -217,10 +184,35 @@ function removeHtmlAttribute(attributesString,attribute) {
 	return attributesString.replace(new RegExp(`\\s*${attribute}="([^"]*)"`),``)
 }
 
-function getFilenameFromUrl(url) {
+async function downloadFileToCache(url,filename,integrity) {
+	let hash,digest
+	if (integrity) {
+		let algorithm
+		[algorithm,digest]=integrity.split('-')
+		hash=createHash(algorithm)
+	}
 	try {
-		const urlObject=new URL(url)
-		if (urlObject.host!='unpkg.com') return
-		return urlObject.pathname.split('/').pop()
-	} catch {}
+		await fs.access(filename)
+	} catch {
+		process.stdout.write(`downloading ${url} `)
+		const buffer=await new Promise((resolve,reject)=>{
+			https.get(url,response=>{
+				if (response.statusCode!==200) return reject(`download of ${filename} failed with status code ${response.statusCode}`)
+				const chunks=[]
+				response.on('data',chunk=>{
+					process.stdout.write(`.`)
+					if (hash) hash.update(chunk)
+					chunks.push(chunk)
+				})
+				response.on('error',err=>reject(`download of ${filename} failed while streaming with error ${err}`))
+				response.on('end',()=>{
+					if (hash && hash.digest('base64')!=digest) reject(`download of ${filename} failed integrity check`)
+					resolve(Buffer.concat(chunks))
+				})
+			})
+		})
+		process.stdout.write(`done\n`)
+		await fs.mkdir(path.dirname(filename),{recursive:true})
+		await fs.writeFile(filename,buffer)
+	}
 }
