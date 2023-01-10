716fe759e6e69464675f6e70a84070d181ffa941
Anton Khorev <tony29@yandex.ru>
2023-01-10 15:09:09 +0300

    browser test for note update

--- test-browser/index.js
+++ test-browser/index.js
@@ -13,36 +13,73 @@ const browserUrl=`${url.pathToFileURL(`${dstDir}/index.html`)}`
 
 describe("browser tests",function(){
 	if (visible) this.timeout(0)
-	let server
 	before(async function(){
 		this.timeout(0)
-		server=await runServer()
-		await buildWithTestServer('src',dstDir,'cache',downloads,server.url)
+		this.server=await runServer()
+		await buildWithTestServer('src',dstDir,'cache',downloads,this.server.url)
 	})
 	after(async function(){
-		await server.close()
+		await this.server.close()
 	})
-	beforeEach(function(){
-		server.clearNotes()
-	})
-	it("runs basic query",async()=>{
-		server.setNotes([{
-			"text": "the-only-note-comment"
-		}])
-		const browser=await puppeteer.launch(visible?{
+	beforeEach(async function(){
+		this.server.clearNotes()
+		this.browser=await puppeteer.launch(visible?{
 			headless: false,
 			slowMo: 300
 		}:{})
-		const page=await browser.newPage()
-		const waitForFetchButton=()=>page.waitForXPath(`//button[not(@disabled) and contains(.,"Fetch notes")]`)
-		const hasText=async(text)=>(await page.$x(`//*[contains(text(),"${text}")]`)).length
-		await page.goto(browserUrl)
-		const button=await waitForFetchButton()
-		assert(!await hasText("the-only-note-comment"))
+		const page=await this.browser.newPage()
+		this.openPage=async()=>{
+			await page.goto(browserUrl)
+			return page
+		}
+		this.waitForFetchButton=()=>page.waitForXPath(`//button[not(@disabled) and contains(.,"Fetch notes")]`)
+		this.hasText=async(text)=>(await page.$x(`//*[contains(text(),"${text}")]`)).length
+	})
+	afterEach(async function(){
+		await this.browser.close()
+	})
+	it("runs basic query",async function(){
+		this.server.setNotes([{
+			"text": "the-only-note-comment"
+		}])
+		const page=await this.openPage()
+		const button=await this.waitForFetchButton()
+		assert(!await this.hasText("the-only-note-comment"))
 		await button.click()
-		await page.waitForSelector(`.notes tbody`)
-		assert(await hasText("the-only-note-comment"))
-		browser.close()
+		await page.waitForSelector('.notes tbody')
+		assert(await this.hasText("the-only-note-comment"))
+	})
+	it("updates the note",async function(){
+		this.server.setNotes([{
+			"id": 101,
+			"comments": [{
+				"date": "2022-04-01",
+				"text": "the-first-note-comment"
+			}]
+		}])
+		const page=await this.openPage()
+		const button=await this.waitForFetchButton()
+		assert(!await this.hasText("the-first-note-comment"))
+		assert(!await this.hasText("the-second-note-comment"))
+		await button.click()
+		await page.waitForSelector('.notes tbody')
+		assert(await this.hasText("the-first-note-comment"))
+		assert(!await this.hasText("the-second-note-comment"))
+		this.server.setNotes([{
+			"id": 101,
+			"comments": [{
+				"date": "2022-04-01",
+				"text": "the-first-note-comment"
+			},{
+				"date": "2022-04-02",
+				"text": "the-second-note-comment"
+			}]
+		}])
+		const updateLink=await page.$(`.notes tbody a`)
+		await updateLink.click()
+		await page.waitForSelector('.notes tbody tr + tr')
+		assert(await this.hasText("the-first-note-comment"))
+		assert(await this.hasText("the-second-note-comment"))
 	})
 })
 
