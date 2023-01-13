3c332b567d19c761ba1c3cd451f40373acd3ca7b
Anton Khorev <tony29@yandex.ru>
2023-01-13 15:11:30 +0300

    interact tool description

--- src/tools/interact.ts
+++ src/tools/interact.ts
@@ -1,23 +1,43 @@
 import {Tool, ToolElements, ToolCallbacks, makeNotesIcon} from './base'
 import type {Note} from '../data'
 import type Server from '../server'
-import {makeEscapeTag} from '../escape'
 import ConfirmedButtonListener from '../confirmed-button-listener'
+import {makeElement, makeLink} from '../html'
+import {makeEscapeTag} from '../escape'
+
+type InfoElements = Array<string|HTMLElement>
+const p=(...ss: InfoElements)=>makeElement('p')()(...ss)
+const em=(s: string)=>makeElement('em')()(s)
+const ul=(...ss: InfoElements)=>makeElement('ul')()(...ss)
+const li=(...ss: InfoElements)=>makeElement('li')()(...ss)
 
 export class InteractTool extends Tool {
 	private selectedNoteIds: ReadonlyArray<number> = []
-	private $windowCountOutput=document.createElement('output')
-	private $confirmWindowCountOutput=document.createElement('output')
+	private $tabCountOutput=document.createElement('output')
+	private $confirmTabCountOutput=document.createElement('output')
 	private reportManyListener?: ConfirmedButtonListener
 	constructor() {super(
 		'interact',
 		`Interact`,
 		`Interact with notes on OSM server`
 	)}
+	getInfo() {return[p(
+		`Currently allows `,makeLink(`reporting`,'https://wiki.openstreetmap.org/wiki/Notes#Reporting_notes'),` selected notes. `,
+		`Since reporting on the OSM website works for individual notes but here you can select many, you may choose between opening one and several tabs.`
+	),ul(
+		li(
+			`If you choose to open one tab, it's going to report the first selected note. `,
+			`The full list of notes will be copied to clipboard for you to paste into the `,em(`details`),` input.`
+		),li(
+			`If you choose to open several tabs, each tab will have a report for every individual note you selected. `,
+			`Since it could be many tabs opened at once, there's a confirmation button appearing for more than five selected notes. `,
+			`Additionally the browser may choose to block opening of new tabs if too many are requested.`
+		)
+	)]}
 	protected onSelectedNotesChangeWithoutHandlingButtons(selectedNotes: ReadonlyArray<Note>): boolean {
 		this.selectedNoteIds=selectedNotes.map(note=>note.id)
 		const count=selectedNotes.length
-		this.$windowCountOutput.textContent=this.$confirmWindowCountOutput.textContent=`${count} window${count==1?'':'s'}`
+		this.$tabCountOutput.textContent=this.$confirmTabCountOutput.textContent=`${count} tab${count==1?'':'s'}`
 		this.reportManyListener?.reset()
 		return true
 	}
@@ -31,9 +51,9 @@ export class InteractTool extends Tool {
 		const $reportManyButton=this.makeRequiringSelectedNotesButton()
 		const $cancelReportManyButton=this.makeRequiringSelectedNotesButton()
 		const $confirmReportManyButton=this.makeRequiringSelectedNotesButton()
-		$reportOneButton.append(`Report `,makeNotesIcon('selected'),` in one window`)
-		$reportManyButton.append(`Report `,makeNotesIcon('selected'),` in `,this.$windowCountOutput)
-		$cancelReportManyButton.append(`Cancel reporting `,makeNotesIcon('selected'),` in `,this.$confirmWindowCountOutput)
+		$reportOneButton.append(`Report `,makeNotesIcon('selected'),` in one tab`)
+		$reportManyButton.append(`Report `,makeNotesIcon('selected'),` in `,this.$tabCountOutput)
+		$cancelReportManyButton.append(`Cancel reporting `,makeNotesIcon('selected'),` in `,this.$confirmTabCountOutput)
 		$confirmReportManyButton.append(`Confirm`)
 		$reportOneButton.onclick=async()=>{
 			await copyNoteList()
@@ -43,7 +63,6 @@ export class InteractTool extends Tool {
 		this.reportManyListener=new ConfirmedButtonListener(
 			$reportManyButton,$cancelReportManyButton,$confirmReportManyButton,
 			async()=>{
-				// TODO write in description that browser might complain about to many opened windows
 				await copyNoteList()
 				for (const id of this.selectedNoteIds) {
 					open(getReportUrl(id))
