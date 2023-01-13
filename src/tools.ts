22801448919e6590d2db608ffa0a5184e6843569
Anton Khorev <tony29@yandex.ru>
2023-01-13 13:21:38 +0300

    basic note reporting

--- src/tools.ts
+++ src/tools.ts
@@ -4,6 +4,7 @@ import {OverpassTurboTool, OverpassTool} from './tools/overpass'
 import * as EditorTools from './tools/editor'
 import * as ExportTools from './tools/export'
 import {StreetViewTool, YandexPanoramasTool, MapillaryTool} from './tools/streetview'
+import {InteractTool} from './tools/interact'
 
 export {
 	Tool, ToolFitMode, ToolCallbacks,
@@ -17,5 +18,6 @@ export const toolMakerSequence: Array<()=>Tool> = [
 	()=>new EditorTools.RcTool, ()=>new EditorTools.IdTool,
 	()=>new ExportTools.GpxTool, ()=>new ExportTools.GeoJsonTool,
 	()=>new YandexPanoramasTool, ()=>new MapillaryTool,
+	()=>new InteractTool,
 	()=>new UtilTools.CountTool, ()=>new UtilTools.LegendTool, ()=>new UtilTools.SettingsTool
 ]
---
+++ src/tools/interact.ts
@@ -0,0 +1,43 @@
+import {Tool, ToolElements, ToolCallbacks, makeNotesIcon} from './base'
+import type {Note} from '../data'
+import type Server from '../server'
+import {makeEscapeTag} from '../escape'
+
+export class InteractTool extends Tool {
+	protected selectedNoteIds: ReadonlyArray<number> = []
+	private $windowCountOutput=document.createElement('output')
+	constructor() {super(
+		'interact',
+		`Interact`,
+		`Interact with notes on OSM server`
+	)}
+	protected onSelectedNotesChangeWithoutHandlingButtons(selectedNotes: ReadonlyArray<Note>): boolean {
+		this.selectedNoteIds=selectedNotes.map(note=>note.id)
+		const count=selectedNotes.length
+		this.$windowCountOutput.textContent=`${count} window${count==1?'':'s'}`
+		return true
+	}
+	getTool(callbacks: ToolCallbacks, server: Server): ToolElements {
+		const e=makeEscapeTag(encodeURIComponent)
+		const getReportUrl=(id:number)=>server.getWebUrl(e`reports/new?reportable_id=${id}&reportable_type=Note`)
+		const $reportOneButton=this.makeRequiringSelectedNotesButton()
+		$reportOneButton.append(`Report `,makeNotesIcon('selected'),` in one window`)
+		$reportOneButton.onclick=()=>{
+			// TODO copy note urls to clipboard
+			const id=this.selectedNoteIds[0]
+			open(getReportUrl(id))
+		}
+		const $reportManyButton=this.makeRequiringSelectedNotesButton()
+		$reportManyButton.append(`Report `,makeNotesIcon('selected'),` in `,this.$windowCountOutput)
+		$reportManyButton.onclick=()=>{
+			// TODO write in description that browser might complain about to many opened windows
+			// TODO warn if opens too many windows
+			for (const id of this.selectedNoteIds) {
+				open(getReportUrl(id))
+			}
+		}
+		return [
+			$reportOneButton,` `,$reportManyButton
+		]
+	}
+}
