35d90375b2527b21ac7f4e61996374a1d8d482a8
Anton Khorev <tony29@yandex.ru>
2023-01-24 19:42:26 +0300

    moved parse tool to its own module

--- src/tools.ts
+++ src/tools.ts
@@ -1,6 +1,7 @@
 import {Tool, ToolFitMode, ToolCallbacks} from './tools/base'
 import * as UtilTools from './tools/util'
 import {RefreshTool} from './tools/refresh'
+import {ParseTool} from './tools/parse'
 import {OverpassTurboTool, OverpassTool} from './tools/overpass'
 import * as EditorTools from './tools/editor'
 import * as ExportTools from './tools/export'
@@ -14,9 +15,8 @@ export {
 
 export const toolMakerSequence: Array<()=>Tool> = [
 	()=>new InteractTool,
-	()=>new UtilTools.AutozoomTool, ()=>new UtilTools.CommentsTool,
-	()=>new RefreshTool,
-	()=>new UtilTools.TimestampTool, ()=>new UtilTools.ParseTool,
+	()=>new UtilTools.AutozoomTool, ()=>new UtilTools.CommentsTool, ()=>new RefreshTool,
+	()=>new UtilTools.TimestampTool, ()=>new ParseTool,
 	()=>new OverpassTurboTool, ()=>new OverpassTool,
 	()=>new EditorTools.RcTool, ()=>new EditorTools.IdTool,
 	()=>new ExportTools.GpxTool, ()=>new ExportTools.GeoJsonTool,
--- src/tools/util.ts
+++ src/tools/util.ts
@@ -1,10 +1,8 @@
 import {Tool, ToolElements, ToolCallbacks, makeNotesIcon, makeMapIcon} from './base'
 import type {Note} from '../data'
-import type Auth from '../auth'
 import type NoteMap from '../map'
-import CommentWriter from '../comment-writer'
 import {makeElement, makeLink} from '../html'
-import {em,dfn,p,ul,li} from '../html-shortcuts'
+import {em,dfn,p} from '../html-shortcuts'
 
 type InfoElements = Array<string|HTMLElement>
 const label=(...ss: InfoElements)=>makeElement('label')('inline')(...ss)
@@ -107,63 +105,6 @@ export class TimestampTool extends Tool {
 	onTimestampChange(timestamp: string): boolean {
 		this.$timestampInput.value=timestamp
 		return true
-	}
-}
-
-export class ParseTool extends Tool {
-	constructor() {super(
-		'parse',
-		`Parse links`,
-		`Extract interactive links from plaintext`
-	)}
-	getInfo() {return[p(
-		`Parse text as if it's a note comment and get its first active element. If such element exists, it's displayed as a link after →. `,
-		`Currently detected active elements are: `,
-	),ul(
-		li(`links to images made in `,makeLink(`StreetComplete`,`https://wiki.openstreetmap.org/wiki/StreetComplete`)),
-		li(`links to OSM notes (clicking the output link is not yet implemented)`),
-		li(`links to OSM changesets`),
-		li(`links to OSM elements`),
-		li(`ISO-formatted timestamps`)
-	),p(
-		`May be useful for displaying an arbitrary OSM element in the map view. Paste the element URL and click the output link.`
-	)]}
-	getTool(callbacks: ToolCallbacks, {server}: Auth): ToolElements {
-		const commentWriter=new CommentWriter(server)
-		const $input=document.createElement('input')
-		$input.type='text'
-		$input.size=50
-		$input.classList.add('complicated')
-		const $parseButton=document.createElement('button')
-		$parseButton.type='submit'
-		$parseButton.textContent='Parse'
-		const $clearButton=document.createElement('button')
-		$clearButton.type='reset'
-		$clearButton.textContent='Clear'
-		const $output=document.createElement('code')
-		$output.append(getFirstActiveElement([]))
-		const $form=makeElement('form')()($input,` `,$parseButton,` `,$clearButton)
-		$form.onsubmit=(ev)=>{
-			ev.preventDefault()
-			const [elements]=commentWriter.makeCommentElements($input.value)
-			$output.replaceChildren(getFirstActiveElement(elements))
-		}
-		$form.onreset=()=>{
-			$output.replaceChildren(getFirstActiveElement([]))
-		}
-		return [$form,` → `,$output]
-		function getFirstActiveElement(elements: Array<string|HTMLAnchorElement|HTMLTimeElement>): string|HTMLElement {
-			for (const element of elements) {
-				if (element instanceof HTMLAnchorElement) {
-					element.textContent=`link`
-					return element
-				} else if (element instanceof HTMLTimeElement) {
-					element.textContent=`date`
-					return element
-				}
-			}
-			return `none`
-		}
 	}
 }
 
