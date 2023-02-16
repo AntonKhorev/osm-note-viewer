ef0453bb86fe97b084939a2fd01230e51e4b6c53
Anton Khorev <tony29@yandex.ru>
2023-02-16 17:00:10 +0300

    block opening user

--- src/tools/report.ts
+++ src/tools/report.ts
@@ -21,16 +21,20 @@ export class ReportTool extends Tool {
 			`Additionally the browser may choose to block opening of new tabs if too many are requested.`
 		)
 	),p(
-		`It's also possible to `,makeLink(`report the user`,'https://wiki.openstreetmap.org/wiki/Report_user'),` that opened the selected notes, if all of them were opened by the same user.`
+		`It's also possible to `,makeLink(`report the user`,'https://wiki.openstreetmap.org/wiki/Report_user'),` that opened the selected notes, if all of them were opened by the same user. `,
+		`For moderators there's a `,makeLink(`block`,'https://wiki.openstreetmap.org/wiki/Data_working_group#User_blocks'),` button. `,
+		`The clipboard is going to contain a list of notes, like when reporting notes.`
 	)]}
 	protected getTool($root: HTMLElement, $tool: HTMLElement): ToolElements {
 		let inputUid: number|undefined
+		let inputUsername: string|undefined
 		let inputNoteIds: ReadonlyArray<number> = []
 		const $tabCountOutput=document.createElement('output')
 		const $confirmTabCountOutput=document.createElement('output')
 		const e=makeEscapeTag(encodeURIComponent)
 		const getNoteReportUrl=(id:number)=>this.auth.server.web.getUrl(e`reports/new?reportable_id=${id}&reportable_type=Note`)
 		const getUserReportUrl=(id:number)=>this.auth.server.web.getUrl(e`reports/new?reportable_id=${id}&reportable_type=User`)
+		const getUserBlockUrl=(username:string)=>this.auth.server.web.getUrl(e`blocks/new/${username}`)
 		const getNoteListItem=(id:number)=>`- `+this.auth.server.web.getUrl(e`note/${id}`)+`\n`
 		const getNoteList=()=>inputNoteIds.map(getNoteListItem).join('')
 		const copyNoteList=()=>navigator.clipboard.writeText(getNoteList())
@@ -39,12 +43,18 @@ export class ReportTool extends Tool {
 		const $cancelReportManyButton=this.makeRequiringSelectedNotesButton()
 		const $confirmReportManyButton=this.makeRequiringSelectedNotesButton()
 		const $reportUserButton=document.createElement('button')
+		const $blockUserButton=document.createElement('button')
 		$reportOneButton.append(`Report `,makeNotesIcon('selected'),` in one tab`)
 		$reportManyButton.append(`Report `,makeNotesIcon('selected'),` in `,$tabCountOutput)
 		$cancelReportManyButton.append(`Cancel reporting `,makeNotesIcon('selected'),` in `,$confirmTabCountOutput)
 		$confirmReportManyButton.append(`Confirm`)
 		$reportUserButton.append(`Report opening user`)
-		$reportUserButton.disabled=true
+		$blockUserButton.append(`Block opening user`)
+		$blockUserButton.disabled=$reportUserButton.disabled=true
+		const updateLoginDependents=()=>{
+			$blockUserButton.hidden=!this.auth.isModerator
+		}
+		updateLoginDependents()
 		$reportOneButton.onclick=async()=>{
 			await copyNoteList()
 			const id=inputNoteIds[0]
@@ -65,19 +75,30 @@ export class ReportTool extends Tool {
 			await copyNoteList()
 			open(getUserReportUrl(inputUid))
 		}
-		$root.addEventListener('osmNoteViewer:changeInputNotes',({detail:[inputNotes]})=>{
+		$blockUserButton.onclick=async()=>{
+			if (inputUsername==null) return
+			await copyNoteList()
+			open(getUserBlockUrl(inputUsername))
+		}
+		$root.addEventListener('osmNoteViewer:changeInputNotes',({detail:[inputNotes,inputUsers]})=>{
 			inputUid=inputNotes[0]?.comments[0]?.uid
-			$reportUserButton.disabled=!(inputUid!=null && inputNotes.every(note=>note.comments[0]?.uid==inputUid))
+			inputUsername=inputUid ? inputUsers.get(inputUid) : undefined
+			$blockUserButton.disabled=$reportUserButton.disabled=!(
+				inputUid!=null && inputNotes.every(note=>note.comments[0]?.uid==inputUid)
+			)
 			inputNoteIds=inputNotes.map(note=>note.id)
 			const count=inputNotes.length
 			$tabCountOutput.textContent=$confirmTabCountOutput.textContent=`${count} tab${count==1?'':'s'}`
 			reportManyListener.reset()
 			this.ping($tool)
 		})
+		$root.addEventListener('osmNoteViewer:changeLogin',()=>{
+			updateLoginDependents()
+		})
 		return [
 			$reportOneButton,` `,
 			$reportManyButton,` `,$cancelReportManyButton,` `,$confirmReportManyButton,` `,
-			$reportUserButton
+			$reportUserButton,` `,$blockUserButton
 		]
 	}
 }
