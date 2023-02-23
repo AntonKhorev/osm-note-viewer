85078a971b3f41b222ec7bf10b1a3baf9b80326f
Anton Khorev <tony29@yandex.ru>
2023-02-24 02:44:40 +0300

    singular id in table caption if there's only one id

--- src/fetch-dialog/plaintext.ts
+++ src/fetch-dialog/plaintext.ts
@@ -78,11 +78,13 @@ export class NotePlaintextFetchDialog extends mixinWithFetchButton(NoteIdsFetchD
 		const showSomeNotesThreshold=5
 		const showAllNotesThreshold=7
 		if (query.mode!='ids') return super.getQueryCaption(query)
-		let ids=`with ids `
-		if (query.ids.length<=showAllNotesThreshold) {
-			ids+=query.ids.join(`, `)
+		let ids: string
+		if (query.ids.length==1) {
+			ids=`with id `+query.ids.join(`, `)
+		} else if (query.ids.length<=showAllNotesThreshold) {
+			ids=`with ids `+query.ids.join(`, `)
 		} else {
-			ids+=query.ids.slice(0,showSomeNotesThreshold).join(`, `)+` and ${query.ids.length-showSomeNotesThreshold} other notes`
+			ids=`with ids `+query.ids.slice(0,showSomeNotesThreshold).join(`, `)+` and ${query.ids.length-showSomeNotesThreshold} other notes`
 		}
 		return makeElement('caption')()(`Fetched notes `,this.makeInputLink(this.$idsTextarea,ids))
 	}
