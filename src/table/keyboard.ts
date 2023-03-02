62bb40f54cb7ae5f77870f30ce1a134aca6bb476
Anton Khorev <tony29@yandex.ru>
2023-03-02 16:32:40 +0300

    up/down keys in table body

--- src/table/keyboard.ts
+++ src/table/keyboard.ts
@@ -36,17 +36,19 @@ export function makeNoteTableKeydownListener(): [
 	},$helpDialog]
 }
 
-const selectors: [headSelector:string,bodySelector:string][] = [
-	['.note-checkbox input','.note-checkbox input'],
-	['.note-link button','.note-link a'],
-	['.note-comments-count button','.note-comments-count button'],
-	['.note-date button','.note-date time'],
-	['.note-user button','.note-user a'],
-	['.note-action','.note-action [class|=icon]'],
-	['.note-comment button','.note-comment']
+const selectors: [spans:boolean,headSelector:string,bodySelector:string][] = [
+	[true,'.note-checkbox input','.note-checkbox input'],
+	[true,'.note-link button','.note-link a'],
+	[true,'.note-comments-count button','.note-comments-count button'],
+	[false,'.note-date button','.note-date time'],
+	[false,'.note-user button','.note-user a'],
+	[false,'.note-action','.note-action [class|=icon]'],
+	[false,'.note-comment button','.note-comment'],
+	[true,'.note-map button','.note-map a'],
 ]
-const HEAD=0
-const BODY=1
+const SPAN=0
+const HEAD=1
+const BODY=2
 
 const tabbableSelector=`a[href]:not([tabindex="-1"]), input:not([tabindex="-1"]), button:not([tabindex="-1"]), [tabindex="0"]`
 
@@ -61,35 +63,37 @@ class KeyboardState {
 		this.iRow=    Number($table.dataset.iKeyboardRow??'0')
 		this.iColumn= Number($table.dataset.iKeyboardColumn??'0')
 	}
-	save() {
+	save(): void {
 		this.$table.dataset.iKeyboardSection=String(this.iSection)
 		this.$table.dataset.iKeyboardRow    =String(this.iRow)
 		this.$table.dataset.iKeyboardColumn =String(this.iColumn)
 	}
-	updateTabIndices() {
+	updateTabIndices(): void {
 		for (const $e of this.$table.querySelectorAll(`:is(thead, tbody) :is(${tabbableSelector})`)) {
 			if ($e instanceof HTMLElement) $e.tabIndex=-1
 		}
-		const $headSection=this.$table.tHead
-		if ($headSection) {
-			const $s=$headSection.querySelector(selectors[this.iColumn][HEAD])
-			if ($s instanceof HTMLElement) $s.tabIndex=0
-		}
-		const $section=this.$table.tBodies[this.iSection]
-		if ($section) {
-			const $row=$section.rows[this.iRow]
-			if (!$row) return
-			const $s=$section.querySelector(selectors[this.iColumn][BODY])
-			if ($s instanceof HTMLElement) $s.tabIndex=0
-		}
+		const $headItem=this.getCurrentHeadItem()
+		if ($headItem) $headItem.tabIndex=0
+		const $bodyItem=this.getCurrentBodyItem()
+		if ($bodyItem) $bodyItem.tabIndex=0
 	}
-	focusInHead() {
-		const $headSection=this.$table.tHead
-		if (!$headSection) return
-		const $s=$headSection.querySelector(selectors[this.iColumn][HEAD])
-		if ($s instanceof HTMLElement) $s.focus()
+	focusInHead(): void {
+		const $item=this.getCurrentHeadItem()
+		$item?.focus()
 	}
-	respondToKeyInHead(key: string) {
+	focusInBody(): void {
+		const $item=this.getCurrentBodyItem()
+		$item?.focus()
+	}
+	respondToKeyInHead(key: string): boolean {
+		return this.respondToHorizontalMovementKey(key)
+	}
+	respondToKeyInBody(key: string): boolean {
+		if (this.respondToHorizontalMovementKey(key)) return true
+		if (this.respondToVerticalMovementKey(key)) return true
+		return false
+	}
+	private respondToHorizontalMovementKey(key: string): boolean {
 		if (key=='ArrowLeft') {
 			if (this.iColumn>0) {
 				this.iColumn--
@@ -109,6 +113,92 @@ class KeyboardState {
 		}
 		return false
 	}
+	private respondToVerticalMovementKey(key: string): boolean {
+		const setSectionAndRowIndicesFromRow=($row:HTMLTableRowElement):boolean=>{
+			const $section=$row.parentElement
+			if (!($section instanceof HTMLTableSectionElement)) return false
+			const iRow=[...$section.rows].indexOf($row)
+			if (iRow<0) return false
+			const iSection=[...this.$table.tBodies].indexOf($section)
+			if (iSection<0) return false
+			this.iRow=iRow
+			this.iSection=iSection
+			return true
+		}
+		const setSectionAndRowIndicesFromSection=($section:HTMLTableSectionElement):boolean=>{
+			const iSection=[...this.$table.tBodies].indexOf($section)
+			if (iSection<0) return false
+			this.iRow=0
+			this.iSection=iSection
+			return true
+		}
+		const moveByRow=(
+			getNextIndex: (i:number)=>number
+		):boolean=>{
+			const $currentRow=this.getCurrentBodyRow()
+			if (!$currentRow) return false
+			const $rows=[...this.$table.querySelectorAll('tbody tr')]
+			let i=$rows.indexOf($currentRow)
+			if (i<0) return false
+			for (i=getNextIndex(i);i>=0&&i<$rows.length;i=getNextIndex(i)) {
+				const $row=$rows[i]
+				if ($row instanceof HTMLTableRowElement && !$row.hidden && !$row.parentElement?.hidden) {
+					return setSectionAndRowIndicesFromRow($row)
+				}
+			}
+			return false
+		}
+		const moveBySection=(
+			getNextIndex: (i:number)=>number
+		):boolean=>{
+			const $currentSection=this.getCurrentBodySection()
+			if (!$currentSection) return false
+			const $sections=[...this.$table.tBodies]
+			let i=$sections.indexOf($currentSection)
+			if (i<0) return false
+			for (i=getNextIndex(i);i>=0&&i<$sections.length;i=getNextIndex(i)) {
+				const $section=$sections[i]
+				if ($section instanceof HTMLTableSectionElement && !$section.hidden) {
+					return setSectionAndRowIndicesFromSection($section)
+				}
+			}
+			return false
+		}
+		if (key=='ArrowUp') {
+			if (selectors[this.iColumn][SPAN]) {
+				return moveBySection(i=>i-1)
+			} else {
+				return moveByRow(i=>i-1)
+			}
+		} else if (key=='ArrowDown') {
+			if (selectors[this.iColumn][SPAN]) {
+				return moveBySection(i=>i+1)
+			} else {
+				return moveByRow(i=>i+1)
+			}
+		}
+		return false
+	}
+	private getCurrentHeadItem(): HTMLElement|null {
+		const $headSection=this.$table.tHead
+		if (!$headSection) return null
+		return $headSection.querySelector(selectors[this.iColumn][HEAD])
+	}
+	private getCurrentBodyItem(): HTMLElement|null {
+		const selector=selectors[this.iColumn][BODY]
+		const $section=this.$table.tBodies.item(this.iSection)
+		if (!$section) return null
+		const $row=$section.rows.item(this.iRow)
+		return $row?.querySelector(selector) ?? $section.querySelector(selector)
+	}
+	private getCurrentBodySection(): HTMLTableSectionElement|null {
+		return this.$table.tBodies.item(this.iSection)
+	}
+	private getCurrentBodyRow(): HTMLTableRowElement|null {
+		const $section=this.getCurrentBodySection()
+		if (!$section) return null
+		return $section.rows.item(this.iRow)
+	}
 }
 
 function noteTableKeydownListener($table: HTMLTableElement, ev: KeyboardEvent): void {
@@ -125,15 +215,17 @@ function noteTableKeydownListener($table: HTMLTableElement, ev: KeyboardEvent): 
 	if (!($section instanceof HTMLTableSectionElement)) return
 	const keyboardState=new KeyboardState($table)
 	if ($section.tagName=='THEAD') {
-		if (keyboardState.respondToKeyInHead(ev.key)) {
-			keyboardState.save()
-			keyboardState.focusInHead()
-			keyboardState.updateTabIndices()
-			ev.stopPropagation()
-			ev.preventDefault()
-			return
-		}
+		if (!keyboardState.respondToKeyInHead(ev.key)) return
+		keyboardState.save()
+		keyboardState.focusInHead()
+	} else {
+		if (!keyboardState.respondToKeyInBody(ev.key)) return
+		keyboardState.save()
+		keyboardState.focusInBody()
 	}
+	keyboardState.updateTabIndices()
+	ev.stopPropagation()
+	ev.preventDefault()
 }
 
 export function noteTableCleanupRovingTabindex($table: HTMLTableElement) {
