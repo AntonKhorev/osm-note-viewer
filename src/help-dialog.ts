78c2e6323c3e7d2ed7aa3d81b9df67805b3456da
Anton Khorev <tony29@yandex.ru>
2023-03-07 14:26:40 +0300

    x close button in help dialog

--- src/help-dialog.ts
+++ src/help-dialog.ts
@@ -3,13 +3,15 @@ import {kbd} from './html-shortcuts'
 
 export default function makeHelpDialog(closeButtonLabel: string, content: (string|HTMLElement)[]): HTMLDialogElement {
 	const $helpDialog=makeElement('dialog')('help')()
-	const $closeButton=makeElement('button')()(closeButtonLabel)
+	const $closeButton=makeElement('button')('close')()
+	$closeButton.title=closeButtonLabel
+	$closeButton.innerHTML=`<svg><use href="#reset" /></svg>`
 	$closeButton.onclick=()=>{
 		$helpDialog.close()
 	}
 	$helpDialog.append(
+		$closeButton,
 		...content,
-		makeDiv('major-input')($closeButton),
 		makeDiv('notice')(`Press `,kbd(`F1`),` again to access the default browser help; press `,kbd(`Esc`),` to close this dialog.`)
 	)
 	return $helpDialog
