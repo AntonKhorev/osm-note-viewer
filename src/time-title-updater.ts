fdda82f2743d895728cc4ac5ae225b015d01d798
Anton Khorev <tony29@yandex.ru>
2023-03-20 02:02:54 +0300

    update time title also on focus

--- src/time-title-updater.ts
+++ src/time-title-updater.ts
@@ -8,23 +8,27 @@ const units=[
 	[60*60*24*365,'year'],
 ] as [duration:number,name:Intl.RelativeTimeFormatUnit][]
 
+const relativeTimeFormat=new Intl.RelativeTimeFormat('en')
+
+function listener(ev: Event) {
+	if (!(ev.target instanceof HTMLTimeElement)) return
+	const $time=ev.target
+	if (!$time.dateTime) return
+	const readableTime=$time.dateTime.replace('T',' ').replace('Z',' UTC')
+	const t1ms=Date.parse($time.dateTime)
+	const t2ms=Date.now()
+	let relativeTime='just now'
+	for (const [duration,name] of units) {
+		if (t2ms-t1ms<duration*1500) break
+		const timeDifferenceInUnits=Math.round((t1ms-t2ms)/(duration*1000))
+		relativeTime=relativeTimeFormat.format(timeDifferenceInUnits,name)
+	}
+	$time.title=`${readableTime}, ${relativeTime}`
+}
+
 export default class TimeTitleUpdater {
 	constructor($root: HTMLElement) {
-		const relativeTimeFormat=new Intl.RelativeTimeFormat('en')
-		$root.addEventListener('mouseover',ev=>{
-			if (!(ev.target instanceof HTMLTimeElement)) return
-			const $time=ev.target
-			if (!$time.dateTime) return
-			const readableTime=$time.dateTime.replace('T',' ').replace('Z',' UTC')
-			const t1ms=Date.parse($time.dateTime)
-			const t2ms=Date.now()
-			let relativeTime='just now'
-			for (const [duration,name] of units) {
-				if (t2ms-t1ms<duration*1500) break
-				const timeDifferenceInUnits=Math.round((t1ms-t2ms)/(duration*1000))
-				relativeTime=relativeTimeFormat.format(timeDifferenceInUnits,name)
-			}
-			$time.title=`${readableTime}, ${relativeTime}`
-		})
+		$root.addEventListener('mouseover',listener)
+		$root.addEventListener('focusin',listener)
 	}
 }
