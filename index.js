0807695d9e52e02a1bbe12a008b72014606d3aff
Anton Khorev <tony29@yandex.ru>
2023-03-22 16:39:00 +0300

    bugfix build

--- index.js
+++ index.js
@@ -10965,9 +10965,18 @@ const units = [
 ];
 const relativeTimeFormat = new Intl.RelativeTimeFormat('en');
 function listener(ev) {
-    if (!(ev.target instanceof HTMLTimeElement))
+    if (!(ev.target instanceof Element))
         return;
-    const $time = ev.target;
+    let $time;
+    if (ev.target instanceof HTMLTimeElement) {
+        $time = ev.target;
+    }
+    else if (ev.target.parentElement instanceof HTMLTimeElement) { // target is <span> inside <time>
+        $time = ev.target.parentElement;
+    }
+    else {
+        return;
+    }
     if (!$time.dateTime)
         return;
     const readableTime = $time.dateTime.replace('T', ' ').replace('Z', ' UTC');
