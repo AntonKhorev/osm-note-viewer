4225886c41b7735c37f446593819492940adbafe
Anton Khorev <tony29@yandex.ru>
2023-04-07 01:45:02 +0300

    swap expander button and column title

--- src/table/section.ts
+++ src/table/section.ts
@@ -16,9 +16,9 @@ export function writeHeadSectionRow(
 	const makeExpanderCell=(cssClass:string,title:string,key:string,clickListener?:(value:number)=>void)=>{
 		const $th=makeElement('th')(cssClass)()
 		const $button=makeExpanderButton(key,clickListener)
-		if (title) $th.append(makeElement('span')('title')(title))
-		if (title && $button) $th.append(` `)
 		if ($button) $th.append($button)
+		if (title && $button) $th.append(` `)
+		if (title) $th.append(makeElement('span')('title')(title))
 		return $th
 	}
 	const $row=$section.insertRow()
