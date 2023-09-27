e878052ffcfcf37ec38a37e0007e58ef8355a4a0
Anton Khorev <tony29@yandex.ru>
2023-09-27 03:46:22 +0300

    separate ids for nominatim forms

--- src/fetch-dialog/bbox-nominatim.ts
+++ src/fetch-dialog/bbox-nominatim.ts
@@ -9,6 +9,8 @@ import {em,code} from '../util/html-shortcuts'
 
 const spanRequest=(...ss: Array<string|HTMLElement>)=>makeElement('span')('advanced-hint')(...ss)
 
+let idCount=0
+
 export default class NominatimSubForm {
 	public $form=document.createElement('form')
 	private $input=document.createElement('input')
@@ -23,20 +25,20 @@ export default class NominatimSubForm {
 		this.bboxFetcher=new NominatimBboxFetcher(
 			nominatim,...makeDumbCache() // TODO real cache in db
 		)
-		this.$form.id='nominatim-form'
+		this.$form.id='nominatim-form-'+idCount++
+		this.$input.type='text'
+		this.$input.required=true
+		this.$input.classList.add('no-invalid-indication') // because it's inside another form that doesn't require it, don't indicate that it's invalid
+		this.$input.name='place'
+		this.$input.setAttribute('form',this.$form.id)
+		this.$button.textContent='Get'
+		this.$button.setAttribute('form',this.$form.id)
 	}
 	write($fieldset: HTMLFieldSetElement): void {
 		$fieldset.append(makeDiv('advanced-hint')(
 			`Make `,makeLink(`Nominatim search query`,`https://nominatim.org/release-docs/develop/api/Search/`),
 			` at `,code(this.nominatim.getSearchUrl(''),em(`parameters`)),`; see `,em(`parameters`),` above and below.`
 		))
-		this.$input.type='text'
-		this.$input.required=true
-		this.$input.classList.add('no-invalid-indication') // because it's inside another form that doesn't require it, don't indicate that it's invalid
-		this.$input.name='place'
-		this.$input.setAttribute('form','nominatim-form')
-		this.$button.textContent='Get'
-		this.$button.setAttribute('form','nominatim-form')
 		$fieldset.append(makeTextButtonInputGroup('spaced')([
 			`Or get bounding box by place name from Nominatim`,spanRequest(` (`,code('q'),` Nominatim parameter)`)
 		],this.$input,this.$button))
